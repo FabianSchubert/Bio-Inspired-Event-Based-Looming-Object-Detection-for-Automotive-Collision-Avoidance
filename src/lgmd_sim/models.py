312845108bce42ac6ecc8a4192dac2f87501dea0
FabianSchubert <fj.schubert@freenet.de>
2024-03-07 14:40:29 +0000

    add specific s neuron model

--- src/lgmd_sim/models.py
+++ src/lgmd_sim/models.py
@@ -5,10 +5,8 @@ import numpy as np
 spike_array_current_source = genn_model.create_custom_current_source_class(
     "",
     param_names=["unit_amplitude"],
-    var_name_types=[("startSpike", "int"),
-                    ("endSpike", "int")],
-    extra_global_params=[("spikeTimes", "scalar*"),
-                         ("spikePolarity", "int8_t*")],
+    var_name_types=[("startSpike", "int"), ("endSpike", "int")],
+    extra_global_params=[("spikeTimes", "scalar*"), ("spikePolarity", "int8_t*")],
     injection_code="""
     if ($(startSpike) != $(endSpike) && $(t) >= $(spikeTimes)[$(startSpike)]) {
         $(injectCurrent, $(unit_amplitude)*$(spikePolarity)[$(startSpike)]);
@@ -23,10 +21,11 @@ spike_array_current_source = genn_model.create_custom_current_source_class(
 bitmask_array_current_source = genn_model.create_custom_current_source_class(
     "",
     param_names=["unit_amplitude"],
-    var_name_types=[("nt", "int"),
-                    ("pop_size", "int")],
-    extra_global_params=[("spikeBitmask", "uint32_t*"),
-                         ("polarityBitmask", "uint32_t*")],
+    var_name_types=[("nt", "int"), ("pop_size", "int")],
+    extra_global_params=[
+        ("spikeBitmask", "uint32_t*"),
+        ("polarityBitmask", "uint32_t*"),
+    ],
     injection_code="""
 
     const unsigned int wordsPerTimestep = (($(pop_size) + 31) / 32);
@@ -40,7 +39,7 @@ bitmask_array_current_source = genn_model.create_custom_current_source_class(
             $(unit_amplitude)*(pol * 2.0 - 1.0)
         );
     }
-    """
+    """,
 )
 
 # LIF neuron model for S neurons with soft reset as in Banchard et al. 2000
@@ -58,7 +57,31 @@ lif_neuron = genn_model.create_custom_neuron_class(
     reset_code="""
     $(V)-= $(V_reset);  // soft reset by $(V_reset)
     """,
-    is_auto_refractory_required=False
+    is_auto_refractory_required=False,
+)
+
+s_neuron = genn_model.create_custom_neuron_class(
+    "S",
+    param_names=["tau_m", "V_thresh", "V_reset"],
+    var_name_types=[
+        ("VI", "scalar"),
+        ("VE", "scalar"),
+        ("V", "scalar"),
+        ("I", "scalar"),
+    ],
+    sim_code="""
+    $(VI) = min(1.0, $(Isyn_I));
+    $(VE) = $(Isyn_E);
+
+    $(I) = $(VE) * (1.0 - $(VI));
+
+    $(V) += ($(I) - $(V)) / $(tau_m) * DT;
+    """,
+    threshold_condition_code="$(V) >= $(V_thresh)",
+    reset_code="""
+    $(V)-= $(V_reset);  // soft reset by $(V_reset)
+    """,
+    is_auto_refractory_required=False,
 )
 
 # connectivity initialisation snippet for 1:1 connectivity between input and S
@@ -81,18 +104,26 @@ one_one_with_boundary = genn_model.create_custom_sparse_connect_init_snippet_cla
         $(endRow);
         """,
     calc_max_row_len_func=genn_model.create_cmlf_class(
-        lambda num_pre, num_post, pars: 1)())
+        lambda num_pre, num_post, pars: 1
+    )(),
+)
 
 # postsynaptic model to emulate the thresholded linear unit F action on LGMD
 threshold_exp_curr = genn_model.create_custom_postsynaptic_class(
     "threshold_exp_curr",
     param_names=["tau", "threshold"],
     derived_params=[
-        ("expDecay", genn_model.create_dpf_class(
-            lambda pars, dt: np.exp(-dt / pars[0]))()),
-        ("init", genn_model.create_dpf_class(lambda pars, dt: (
-            pars[0] * (1.0 - np.exp(-dt / pars[0]))) * (1.0 / dt))()),
+        (
+            "expDecay",
+            genn_model.create_dpf_class(lambda pars, dt: np.exp(-dt / pars[0]))(),
+        ),
+        (
+            "init",
+            genn_model.create_dpf_class(
+                lambda pars, dt: (pars[0] * (1.0 - np.exp(-dt / pars[0]))) * (1.0 / dt)
+            )(),
+        ),
     ],
     decay_code="$(inSyn)*= $(expDecay);",
-    apply_input_code="if ($(init)*$(inSyn) < -$(threshold)) $(Isyn)+= $(init)*$(inSyn);"
+    apply_input_code="if ($(init)*$(inSyn) < -$(threshold)) $(Isyn)+= $(init)*$(inSyn);",
 )
